d200821bdebeb9319fb0ff186e2b298a71a376ce
Jannatul Ferdous <jannatul0010@gmail.com>
2023-03-22 14:44:53 +0600

    first commit

---
+++ src/test/java/jfl/training/selenium/ChromeBrowserLaunch.java
@@ -0,0 +1,22 @@
+package jfl.training.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class ChromeBrowserLaunch {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver chromeDriver = new ChromeDriver(options);
+
+        chromeDriver.get("https://www.aiub.edu/");
+        chromeDriver.manage().window().maximize();
+        System.out.println("Webpage title is: " + chromeDriver.getTitle());
+        System.out.println("Current page's URL is: " + chromeDriver.getCurrentUrl());
+        chromeDriver.quit();
+    }
+}
---
+++ src/test/java/jfl/training/selenium/FirefoxBrowserLaunch.java
@@ -0,0 +1,18 @@
+package jfl.training.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxBrowserLaunch {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver firefoxDriver = new FirefoxDriver();
+
+        firefoxDriver.get("https://www.aiub.edu/");
+        firefoxDriver.manage().window().maximize();
+        System.out.println("Webpage title is: " + firefoxDriver.getTitle());
+        System.out.println("Current page's URL is: " + firefoxDriver.getCurrentUrl());
+        firefoxDriver.quit();
+    }
+}
